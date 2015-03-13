d2109fdbedff748d5be96329dee898466f6fc527
Abhisek Kundu <abhisek.kundu@innofied.com>
2015-03-14 01:27:17 +0530

    create some common html5 required and validate regexp

--- test.js
+++ test.js
@@ -6,8 +6,17 @@ angular
 controller.$inject = ['$scope'];
 
 function controller($scope) {
+
   $scope.pattern = /^a$/i;
+  $scope.minLength = 2;
+  $scope.maxLength = 3;
+
+  // possible length values
+  // $scope.length = [1, 2];
+  $scope.length = [3, 2];
+
   $scope.submitForm = function() {
     window.alert('submited');
   };
+
 }
